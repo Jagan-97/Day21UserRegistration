a2e99a386fea9f3d1177fc5fd3d401844e3b5f61
Jagan-97 <jagan.psaran38@gmail.com>
2022-11-09 21:10:16 +0530

    added lastename_validate logic

--- src/main/java/org/example/UserRegistration/UserRegistration.java
+++ src/main/java/org/example/UserRegistration/UserRegistration.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegistrationUC1{
+public class UserRegistration {
     static Scanner sc = new Scanner(System.in);
     public static void validateFirstName() {
         System.out.println("Enter First name ");
@@ -17,9 +17,22 @@ public class UserRegistrationUC1{
             System.out.println("Entered Name is Invalid");
         }
     }
+    public static void validateLastName() {
+        System.out.println("Enter Last name ");
+        String last_name = sc.nextLine();
+        String lastNamePattern = "^[A-Z]{1}[a-z,A-Z]{2,}";
+        Pattern pattern = Pattern.compile(lastNamePattern);
+        Matcher matcher = pattern.matcher(last_name);
+        if (matcher.matches()) {
+            System.out.println("Entered Name is Valid");
+        } else {
+            System.out.println("Entered Name is Not Valid");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Project ");
         validateFirstName();
+        validateLastName();
     }
 }
 
