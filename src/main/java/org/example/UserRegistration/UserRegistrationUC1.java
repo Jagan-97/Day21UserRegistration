6ea0afe79b58478215550001b458ee0457b899da
Jagan-97 <jagan.psaran38@gmail.com>
2022-11-10 16:44:49 +0530

    Added email_validate logic

--- src/main/java/org/example/UserRegistration/UserRegistrationUC1.java
+++ src/main/java/org/example/UserRegistration/UserRegistrationUC1.java
@@ -17,9 +17,37 @@ public class UserRegistrationUC1{
             System.out.println("Entered Name is Invalid");
         }
     }
+
+    public static void validateLastName() {
+        System.out.println("Enter Last name ");
+        String last_name = sc.nextLine();
+        String lastNamePattern = "[A-Z]{1}[a-z,A-Z]{2,}";
+        Pattern pattern = Pattern.compile(lastNamePattern);
+        Matcher matcher = pattern.matcher(last_name);
+        if (matcher.matches()) {
+            System.out.println("Entered Name is Valid");
+        } else {
+            System.out.println("Entered Name is Invalid");
+        }
+    }
+    public static void validateEmail () {
+        System.out.println("Enter email ");
+        String email = sc.nextLine();
+        String emailPattern = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*[@]{1}[a-z]+[.]{1}[a-z]{2,4}[.]*[a-z]{2}$";
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches()) {
+            System.out.println("Entered ID is Valid");
+        } else {
+            System.out.println("Entered ID is Invalid");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Project ");
         validateFirstName();
+        validateLastName();
+        validateEmail();
     }
 }
 
